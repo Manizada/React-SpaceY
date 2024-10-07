d31d52c3eb9a61fb9e38c9e0792527e744c7e867
Geovani <maniyt60@gmail.com>
2024-10-07 10:38:12 -0400

    Header added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import './Home.css'
 import Card from '../../components/card/Card'
+import Header from '../../components/header/Header'
 
 function Home() {
     return (
         <>
+            <Header/>
             <div className="container">
                 <Card photo={'/images/solpequeno.png'} photosub={'Sol'} title={'Sol'} subscription={'Não sei o que escrever mas preciso preencher este campo para testar'}/>
             </div>
